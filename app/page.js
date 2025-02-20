93e8bab85bc1d8cf30849bd93695048fe103c394
Drkadam07 <drkadam04@gmail.com>
2025-02-20 16:23:46 +0530

    Added componets

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
+import TopBar from "@/Components/ui/TopBar";
 import Hero from "./home/page";
+import Header from "@/Components/Header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Hero/>
+    <div className="min-h-screen">
+      <TopBar/>
+      <Header/>
+      <main>
+        <Hero/>
+      </main>
+      
     </div>
   );
 }
