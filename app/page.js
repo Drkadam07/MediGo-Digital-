d682fa7af5251283be34e49084dda530ff59aac7
Drkadam07 <drkadam04@gmail.com>
2025-02-20 18:58:14 +0530

    removed header

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 import Hero from "./home/page";
-import Header from "@/Components/Header";
 import About from "./about/page";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <Header/>
       <main>
         <Hero />
         <About/>
