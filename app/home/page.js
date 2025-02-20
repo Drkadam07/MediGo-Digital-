62ff1a20640ec617181083725b62c06053f342a5
Drkadam07 <drkadam04@gmail.com>
2025-02-20 18:29:41 +0530

    chnage text

--- app/home/page.js
+++ app/home/page.js
@@ -4,13 +4,13 @@ export default function Hero() {
             <div className="absolute inset-0">
                 <div className="absolute inset-0 bg-[url('/herobg.png')] opacity-15"></div>
             </div>
-            <div className="container mx-auto px-4 py-20 relative">
+            <div className="container mx-auto px-4 py-20 relative mt-10">
                 <div className="grid lg:grid-cols-2 gap-12 items-center">
                     <div className="space-y-8">
-                        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
-                            The prescription for Digital Success.
+                        <h1 className="text-4xl font-semibold md:text-5xl lg:text-6xl font-bold leading-tight">
+                            The Prescription For Digital Success.
                         </h1>
-                        <p className="text-xl text-gray-800">
+                        <p className="text-xl text-gray-800 ">
                             Transforming Healthcare Through Digital Innovation
                         </p>
                         <button className="bg-gradient-to-r rounded-full from-white via-[#add8e6] to-[#f8d7da] hover:bg-gradient-to-r hover:from-[#add8e6] hover:to-white text-black text-lg px-6 py-3 w-auto">
