8b30eb13fc17b935e36c13e1d14cf4aa55ac2f83
Drkadam07 <drkadam04@gmail.com>
2025-02-21 17:15:10 +0530

    Added sevices FQA contact etc

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Hero from "./home/page";
 import About from "./about/page";
+import Services from "./services/page";
+import HealthcareSpecialists from "./industries/page";
+import FAQSection from "./Components/FAQSection";
+import Contact from "./contactus/page";
 
 export default function Home() {
   return (
@@ -7,6 +11,10 @@ export default function Home() {
       <main>
         <Hero />
         <About/>
+        <Services/>
+        <HealthcareSpecialists/>
+        <FAQSection/>
+        <Contact/>
       </main>
     </div>
   );
